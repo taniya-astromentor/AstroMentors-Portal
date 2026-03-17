a2a2693a2bdb9d4ea46fb6afce8652d327a7ab43
Abhishek <13.tech.ak@gmail.com>
2026-03-18 00:46:26 +0530

    created index.html and added audio assets

---
+++ database.js
@@ -0,0 +1,16 @@
+/**
+ * AstroMentors Tarot Database
+ */
+const tarotDatabase = {
+  "the-fool": {
+    title: "The Fool",
+    image: "images/image1.png",
+    audioPath: "audio/major_arcana/0_the_fool.mp3",
+  },
+  "high-priestess": {
+    title: "The High Priestess",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/8/8d/RWS_Tarot_02_High_Priestess.jpg",
+    audioPath: "audio/major_arcana/2_the_high_priestess.mp3",
+  },
+};
