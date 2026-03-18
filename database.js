d4d6080239ab32543e56d6624eda20d776360317
Abhishek <13.tech.ak@gmail.com>
2026-03-19 01:04:18 +0530

    added image assets and updated database.js

--- database.js
+++ database.js
@@ -2,15 +2,413 @@
  * AstroMentors Tarot Database
  */
 const tarotDatabase = {
+  // major arcana
   "the-fool": {
     title: "The Fool",
-    image: "images/image1.png",
+    image: "images/major_arcana/the_fool.png",
     audioPath: "audio/major_arcana/0_the_fool.mp3",
+  },
+  "the-magician": {
+    title: "The Magician",
+    image: "images/major_arcana/the_magician.png",
+    audioPath: "audio/major_arcana/1_the_magician.mp3",
   },
   "high-priestess": {
     title: "The High Priestess",
-    image:
-      "https://upload.wikimedia.org/wikipedia/en/8/8d/RWS_Tarot_02_High_Priestess.jpg",
+    image: "images/major_arcana/the_high_priestess.png",
     audioPath: "audio/major_arcana/2_the_high_priestess.mp3",
   },
+  "the-empress": {
+    title: "The Empress",
+    image: "images/major_arcana/the_empress.png",
+    audioPath: "audio/major_arcana/3_the_empress.mp3",
+  },
+  "the-emperor": {
+    title: "The Emperor",
+    image: "images/major_arcana/the_emperor.png",
+    audioPath: "audio/major_arcana/4_the_emperor.mp3",
+  },
+  "the-hierophant": {
+    title: "The Hierophant",
+    image: "images/major_arcana/the_hierophant.png",
+    audioPath: "audio/major_arcana/5_the_hierophant.mp3",
+  },
+  "the-lovers": {
+    title: "The Lovers",
+    image: "images/major_arcana/the_lovers.png",
+    audioPath: "audio/major_arcana/6_the_lovers.mp3",
+  },
+  "the-chariot": {
+    title: "The Chariot",
+    image: "images/major_arcana/the_chariot.png",
+    audioPath: "audio/major_arcana/7_the_chariot.mp3",
+  },
+  strength: {
+    title: "Strength",
+    image: "images/major_arcana/strength.png",
+    audioPath: "audio/major_arcana/8_strength.mp3",
+  },
+  "the-hermit": {
+    title: "The Hermit",
+    image: "images/major_arcana/the_hermit.png",
+    audioPath: "audio/major_arcana/9_the_hermit.mp3",
+  },
+  "wheel-of-fortune": {
+    title: "Wheel of Fortune",
+    image: "images/major_arcana/wheel_of_fortune.png",
+    audioPath: "audio/major_arcana/10_wheel_of_fortune.mp3",
+  },
+  justice: {
+    title: "Justice",
+    image: "images/major_arcana/justice.png",
+    audioPath: "audio/major_arcana/11_justice.mp3",
+  },
+  "the-hangedman": {
+    title: "The Hanged Man",
+    image: "images/major_arcana/the_hanged_man.png",
+    audioPath: "audio/major_arcana/12_the_hanged_man.mp3",
+  },
+  death: {
+    title: "Death",
+    image: "images/major_arcana/death.png",
+    audioPath: "audio/major_arcana/13_Death.mp3",
+  },
+  temperance: {
+    title: "Temperance",
+    image: "images/major_arcana/temperance.png",
+    audioPath: "audio/major_arcana/14_temperance.mp3",
+  },
+  "the-fool": {
+    title: "The Fool",
+    image: "images/the_fool.png",
+    audioPath: "audio/major_arcana/0_the_fool.mp3",
+  },
+  "the-devil": {
+    title: "The Devil",
+    image: "images/major_arcana/the_devil.png",
+    audioPath: "audio/major_arcana/15_the_devil.mp3",
+  },
+  "the-tower": {
+    title: "The Tower",
+    image: "images/major_arcana/the_tower.png",
+    audioPath: "audio/major_arcana/16_the_tower.mp3",
+  },
+  "the-star": {
+    title: "The Star",
+    image: "images/major_arcana/the_star.png",
+    audioPath: "audio/major_arcana/17_the_star.mp3",
+  },
+  "the-moon": {
+    title: "The Moon",
+    image: "images/major_arcana/the_moon.png",
+    audioPath: "audio/major_arcana/18_the_moon.mp3",
+  },
+  "the-sun": {
+    title: "The Sun",
+    image: "images/major_arcana/the_sun.png",
+    audioPath: "audio/major_arcana/19_the_sun.mp3",
+  },
+  judgement: {
+    title: "Judgement",
+    image: "images/major_arcana/judgement.png",
+    audioPath: "audio/major_arcana/20_judgement.mp3",
+  },
+  "the-world": {
+    title: "The World",
+    image: "images/major_arcana/the_world.png",
+    audioPath: "audio/major_arcana/21_the_world.mp3",
+  },
+
+  // suit of pentacles
+
+  "ace-of-pentacles": {
+    title: "Ace of Pentacles",
+    image: "images/suit_of_pentacles/ace_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/1_ace_of_pentacles.mp3",
+  },
+  "two-of-pentacles": {
+    title: "Two of Pentacles",
+    image: "images/suit_of_pentacles/two_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/2_two_of_pentacles.mp3",
+  },
+  "three-of-pentacles": {
+    title: "Three of Pentacles",
+    image: "images/suit_of_pentacles/three_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/3_three_of_pentacles.mp3",
+  },
+  "four-of-pentacles": {
+    title: "Four of Pentacles",
+    image: "images/suit_of_pentacles/four_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/4_four_of_pentacles.mp3",
+  },
+  "five-of-pentacles": {
+    title: "Five of Pentacles",
+    image: "images/suit_of_pentacles/five_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/5_five_of_pentacles.mp3",
+  },
+  "six-of-pentacles": {
+    title: "Six of Pentacles",
+    image: "images/suit_of_pentacles/six_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/6_six_of_pentacles.mp3",
+  },
+  "seven-of-pentacles": {
+    title: "Seven of Pentacles",
+    image: "images/suit_of_pentacles/seven_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/7_seven_of_pentacles.mp3",
+  },
+  "eight-of-pentacles": {
+    title: "Eight of Pentacles",
+    image: "images/suit_of_pentacles/eight_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/8_eight_of_pentacles.mp3",
+  },
+  "nine-of-pentacles": {
+    title: "Nine of Pentacles",
+    image: "images/suit_of_pentacles/nine_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/9_nine_of_pentacles.mp3",
+  },
+  "ten-of-pentacles": {
+    title: "Ten of Pentacles",
+    image: "images/suit_of_pentacles/ten_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/10_ten_of_pentacles.mp3",
+  },
+  "page-of-pentacles": {
+    title: "Page of Pentacles",
+    image: "images/suit_of_pentacles/page_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/11_page_of_pentacles.mp3",
+  },
+  "knight-of-pentacles": {
+    title: "Knight of Pentacles",
+    image: "images/suit_of_pentacles/knight_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/12_knight_of_pentacles.mp3",
+  },
+  "queen-of-pentacles": {
+    title: "Queen of Pentacles",
+    image: "images/suit_of_pentacles/queen_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/13_queen_of_pentacles.mp3",
+  },
+  "king-of-pentacles": {
+    title: "King of Pentacles",
+    image: "images/suit_of_pentacles/king_of_pentacles.png",
+    audioPath: "audio/suit_of_pentacles/14_king_of_pentacles.mp3",
+  },
+
+  // suit of cups
+
+  "ace-of-cups": {
+    title: "Ace of Cups",
+    image: "images/suit_of_cups/ace_of_cups.png",
+    audioPath: "audio/suit_of_cups/1_Ace_of_cups.mp3",
+  },
+  "two-of-cups": {
+    title: "Two of Cups",
+    image: "images/suit_of_cups/two_of_cups.png",
+    audioPath: "audio/suit_of_cups/2_two_of_cups.mp3",
+  },
+  "three-of-cups": {
+    title: "Three of Cups",
+    image: "images/suit_of_cups/three_of_cups.png",
+    audioPath: "audio/suit_of_cups/3_three_of_cups.mp3",
+  },
+  "four-of-cups": {
+    title: "Four of Cups",
+    image: "images/suit_of_cups/four_of_cups.png",
+    audioPath: "audio/suit_of_cups/4_four_of_cups.mp3",
+  },
+  "five-of-cups": {
+    title: "Five of Cups",
+    image: "images/suit_of_cups/five_of_cups.png",
+    audioPath: "audio/suit_of_cups/5_five_of_cups.mp3",
+  },
+  "six-of-cups": {
+    title: "Six of Cups",
+    image: "images/suit_of_cups/six_of_cups.png",
+    audioPath: "audio/suit_of_cups/6_six_of_cups.mp3",
+  },
+  "seven-of-cups": {
+    title: "Seven of Cups",
+    image: "images/suit_of_cups/seven_of_cups.png",
+    audioPath: "audio/suit_of_cups/7_seven_of_cups.mp3",
+  },
+  "eight-of-cups": {
+    title: "Eight of Cups",
+    image: "images/suit_of_cups/eight_of_cups.png",
+    audioPath: "audio/suit_of_cups/8_eight_of_cups.mp3",
+  },
+  "nine-of-cups": {
+    title: "Nine of Cups",
+    image: "images/suit_of_cups/nine_of_cups.png",
+    audioPath: "audio/suit_of_cups/9_nine_of_cups.mp3",
+  },
+  "ten-of-cups": {
+    title: "Ten of Cups",
+    image: "images/suit_of_cups/ten_of_cups.png",
+    audioPath: "audio/suit_of_cups/10_ten_of_cups.mp3",
+  },
+  "page-of-cups": {
+    title: "Page of Cups",
+    image: "images/suit_of_cups/page_of_cups.png",
+    audioPath: "audio/suit_of_cups/11_page_of_cups.mp3",
+  },
+  "knight-of-cups": {
+    title: "Knight of Cups",
+    image: "images/suit_of_cups/knight_of_cups.png",
+    audioPath: "audio/suit_of_cups/12_knight_of_cups.mp3",
+  },
+  "queen-of-cups": {
+    title: "Queen of Cups",
+    image: "images/suit_of_cups/queen_of_cups.png",
+    audioPath: "audio/suit_of_cups/13_queen_of_cups.mp3",
+  },
+  "king-of-cups": {
+    title: "King of Cups",
+    image: "images/suit_of_cups/king_of_cups.png",
+    audioPath: "audio/suit_of_cups/14_king_of_cups.mp3",
+  },
+
+  //suit of wands
+
+  "ace-of-wands": {
+    title: "Ace of Wands",
+    image: "images/suit_of_wands/ace_of_wands.png",
+    audioPath: "audio/suit_of_wands/1_ace_of_wands.mp3",
+  },
+  "two-of-wands": {
+    title: "Two of Wands",
+    image: "images/suit_of_wands/two_of_wands.png",
+    audioPath: "audio/suit_of_wands/2_two_of_wands.mp3",
+  },
+  "three-of-wands": {
+    title: "Three of Wands",
+    image: "images/suit_of_wands/three_of_wands.png",
+    audioPath: "",
+  },
+  "four-of-wands": {
+    title: "Four of Wands",
+    image: "images/suit_of_wands/four_of_wands.png",
+    audioPath: "audio/suit_of_wands/4_four_of_wands.mp3",
+  },
+  "five-of-wands": {
+    title: "Five of Wands",
+    image: "images/suit_of_wands/five_of_wands.png",
+    audioPath: "",
+  },
+  "six-of-wands": {
+    title: "Six of Wands",
+    image: "images/suit_of_wands/six_of_wands.png",
+    audioPath: "audio/suit_of_wands/6_six_of_wands.mp3",
+  },
+  "seven-of-wands": {
+    title: "Seven of Wands",
+    image: "images/suit_of_wands/seven_of_wands.png",
+    audioPath: "audio/suit_of_wands/7_seven_of_wands.mp3",
+  },
+  "eight-of-wands": {
+    title: "Eight of Wands",
+    image: "images/suit_of_wands/eight_of_wands.png",
+    audioPath: "audio/suit_of_wands/8_eight_of_wands.mp3",
+  },
+  "nine-of-wands": {
+    title: "Nine of Wands",
+    image: "images/suit_of_wands/nine_of_wands.png",
+    audioPath: "audio/suit_of_wands/9_nine_of_wands.mp3",
+  },
+  "ten-of-wands": {
+    title: "Ten of Wands",
+    image: "images/suit_of_wands/ten_of_wands.png",
+    audioPath: "audio/suit_of_wands/10_ten_of_wands.mp3",
+  },
+  "page-of-wands": {
+    title: "Page of Wands",
+    image: "images/suit_of_wands/page_of_wands.png",
+    audioPath: "audio/suit_of_wands/11_page_of_wands.mp3",
+  },
+  "knight-of-wands": {
+    title: "Knight of Wands",
+    image: "images/suit_of_wands/knight_of_wands.png",
+    audioPath: "audio/suit_of_wands/12_knight_of_wands.mp3",
+  },
+  "queen-of-wands": {
+    title: "Queen of Wands",
+    image: "images/suit_of_wands/queen_of_wands.png",
+    audioPath: "audio/suit_of_wands/13_queen_of_wands.mp3",
+  },
+  "king-of-wands": {
+    title: "King of Wands",
+    image: "images/suit_of_wands/king_of_wands.png",
+    audioPath: "audio/suit_of_wands/14_king_of_wands.mp3",
+  },
+
+  //suit of swords
+
+  "ace-of-swords": {
+    title: "Ace of Swords",
+    image: "images/suit_of_swords/ace_of_swords.png",
+    audioPath: "audio/suit_of_swords/1_ace_of_swords.mp3",
+  },
+  "two-of-swords": {
+    title: "Two of Swords",
+    image: "images/suit_of_swords/two_of_swords.png",
+    audioPath: "audio/suit_of_swords/2_two_of_swords.mp3",
+  },
+  "three-of-swords": {
+    title: "Three of Swords",
+    image: "images/suit_of_swords/three_of_swords.png",
+    audioPath: "audio/suit_of_swords/3_three_of_swords.mp3",
+  },
+  "four-of-swords": {
+    title: "Four of Swords",
+    image: "images/suit_of_swords/four_of_swords.png",
+    audioPath: "audio/suit_of_swords/4_four_of_swords.mp3",
+  },
+  "five-of-swords": {
+    title: "Five of Swords",
+    image: "images/suit_of_swords/five_of_swords.png",
+    audioPath: "audio/suit_of_swords/5_five_of_swords.mp3",
+  },
+  "six-of-swords": {
+    title: "Six of Swords",
+    image: "images/suit_of_swords/six_of_swords.png",
+    audioPath: "audio/suit_of_swords/6_six_of_swords.mp3",
+  },
+  "seven-of-swords": {
+    title: "Seven of Swords",
+    image: "images/suit_of_swords/seven_of_swords.png",
+    audioPath: "audio/suit_of_swords/7_seven_of_swords.mp3",
+  },
+  "eight-of-swords": {
+    title: "Eight of Swords",
+    image: "images/suit_of_swords/eight_of_swords.png",
+    audioPath: "audio/suit_of_swords/8_eight_of_swords.mp3",
+  },
+  "nine-of-swords": {
+    title: "Nine of Swords",
+    image: "images/suit_of_swords/nine_of_swords.png",
+    audioPath: "audio/suit_of_swords/9_nine_of_swords.mp3",
+  },
+  "ten-of-swords": {
+    title: "Ten of Swords",
+    image: "images/suit_of_swords/ten_of_swords.png",
+    audioPath: "audio/suit_of_swords/10_ten_of_swords.mp3",
+  },
+  "page-of-swords": {
+    title: "Page of Swords",
+    image: "images/suit_of_swords/page_of_swords.png",
+    audioPath: "audio/suit_of_swords/11_page_of_swords.mp3",
+  },
+  "knight-of-swords": {
+    title: "Knight of Swords",
+    image: "images/suit_of_swords/knight_of_swords.png",
+    audioPath: "audio/suit_of_swords/12_knight_of_swords.mp3",
+  },
+  "queen-of-swords": {
+    title: "Queen of Swords",
+    image: "images/suit_of_swords/queen_of_swords.png",
+    audioPath: "audio/suit_of_swords/13_queen_of_swords.mp3",
+  },
+  "king-of-swords": {
+    title: "King of Swords",
+    image: "images/suit_of_swords/king_of_swords.png",
+    audioPath: "audio/suit_of_swords/14_king_of_swords.mp3",
+  },
+  //
 };
